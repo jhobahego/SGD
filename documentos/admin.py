ae58df723a6d7cddf91997c8456208a162f4de45
Jhon Hernandez <jhobahego16@hotmail.com>
2022-10-06 19:44:39 -0500

    Primer commit

--- documentos/admin.py
+++ documentos/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Usuarios, Roles
+from .models import Usuarios, Roles, Documento, Registro
 
 # Register your models here.
 admin.site.register(Usuarios)
 admin.site.register(Roles)
+admin.site.register(Documento)
+admin.site.register(Registro)
