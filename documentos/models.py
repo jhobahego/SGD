d869baf712d10d43ea5645fa518d997445ada9bc
Jhon Hernandez <jhobahego16@hotmail.com>
2022-10-11 18:26:02 -0500

    Campos agregados a tabla documentos y funcion str

--- documentos/models.py
+++ documentos/models.py
@@ -1,4 +1,5 @@
 from email.policy import default
+from hashlib import blake2b
 from django.db import models
 
 # Create your models here.
@@ -10,9 +11,15 @@ class Usuarios(models.Model):
     contra = models.CharField(max_length=40)
     telefono = models.CharField(max_length=15)
 
+    def __str__(self) -> str:
+        return self.nombres, self.apellidos
+
 class Roles(models.Model):
     nombre_rol = models.CharField(max_length=40)
     usuario_id = models.OneToOneField(Usuarios, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.nombre_rol
 
 class Documento(models.Model):
     autor = models.CharField(max_length=40)
@@ -22,9 +29,18 @@ class Documento(models.Model):
     categoria = models.CharField(max_length=40)
     stock = models.IntegerField()
     precio = models.IntegerField()
+    editorial = models.CharField(max_length=40, null=True, blank=True)
+    idioma = models.CharField(max_length=15, null=True, blank=True)
+    paginas = models.IntegerField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.categoria
 
 class Registro(models.Model):
     usuario_id = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
     documento_id = models.ForeignKey(Documento, on_delete=models.CASCADE)
     tipo_venta = models.CharField(max_length=40)
     cantidad = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.tipo_venta
